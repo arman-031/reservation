28bd9037cb8eea8a14ac2d4b17ffb707efbd8221
arman <armanrezagholian>
2026-09-12 11:00:32 +0330

    Initial reservation backend setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Reservation
+
+A Django reservation backend currently under development.
+
+## Status
+
+This project is actively being developed. The API, reservation workflow, validation, authentication, and production configuration are not complete yet.
+
+## Development
+
+The project uses Django with the `turns` application. More documentation will be added as the reservation domain and API stabilize.
---
+++ turns/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class ReservationDay(models.Model):
+    date = models.DateField(_('Date'))
+
+    class Meta:
+        verbose_name = _('روز')
+        verbose_name_plural=('روزها')
+        ordering = ('-date',)
+
+    def __str__(self):
+        return f"(self.date)"
+
+class Reservation(models.Model):
+    dey=models.ForeignKey(ReservationDay,on_delete=models.CASCADE,verbose_name=_('روز'),related_name='reservations')
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True,verbose_name=_('user'))
+    time = models.TimeField(_('Time'))
+
+    class Meta:
+        verbose_name = _('رزرو')
+        verbose_name_plural = ('رزروها')
+        ordering = ('time',)
+
+    def __str__(self):
+        return f"(self.user)-(self.dey)-(self.time)"
